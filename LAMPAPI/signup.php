9fe4a8a4a5f25b93eea8f63952047f22f8c9d65b
Nelson Oliva <nelson.ricardo02@gmail.com>
2025-02-09 18:21:54 -0500

    added code to test signup

---
+++ LAMPAPI/signup.php
@@ -0,0 +1,12 @@
+<?php 
+    // read the data http body into $in_data
+    $in_data = json_decode(file_get_contents('php://input'), true);
+    
+    // make a connection to the database
+    $db_connection = new mysqli("localhost", "admins", "administratorpriveleges", "Contact");
+    if( $db_connection->connect_error ) {
+        header('Content-type application/json');
+        $error_json_msg = '{"msg":"WOMP WOMP"}';
+        echo $error_json_msg;
+    }
+?>
