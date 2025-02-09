100da4fc91af36bee80bec09c6307544a7ee8462
Nelson Oliva <nelson.ricardo02@gmail.com>
2025-02-09 18:34:51 -0500

    fixed sql connection

--- LAMPAPI/signup.php
+++ LAMPAPI/signup.php
@@ -3,7 +3,7 @@
     $in_data = json_decode(file_get_contents('php://input'), true);
     
     // make a connection to the database
-    $db_connection = new mysqli("localhost", "admins", "administratorpriveleges", "Contact");
+    $db_connection = new mysqli("localhost", "admin", "administratorpriveleges", "Contact");
     if( $db_connection->connect_error ) {
         header('Content-type: application/json');
         $error_json_msg = '{"msg":"WOMP WOMP"}';
