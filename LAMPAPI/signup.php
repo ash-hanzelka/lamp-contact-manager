448b90eb51d1c71271db5236cbe1a1dd3da6a281
Nelson Oliva <nelson.ricardo02@gmail.com>
2025-02-09 20:05:36 -0500

    check if user exists in the database

--- LAMPAPI/signup.php
+++ LAMPAPI/signup.php
@@ -6,16 +6,37 @@
 
     // make a connection to the database
     $db_connection = new mysqli("localhost", "Admin", "administratorpriveleges", "Contact");
+
     if( $db_connection->connect_error ) {
-        header('Content-type: application/json');
         $error_json_msg = '{"msg":"WOMP WOMP"}';
-        echo $error_json_msg;
+        returnJson($error_json_msg);
     } else {
+        $existence_query = sprintf("SELECT COUNT(*) AS num_users FROM Users WHERE username = %s", $username);
+        $existence_stmt = $db_connection->prepare($existence_query);
+        if(!$existence_stmt->execute()) {
+            $failure_msg = '{"msg":"sql failed idk sowwy! >_<"}';
+            returnJson($failure_msg);
+            die(1); // cooler version of exit
+        }
+        $existence_result = $existence_stmt->get_result();
+        $num_users = (int) $existence_result->fetch_assoc()["num_users"];
+
+        if($num_users == 1) {
+            $user_exists_msg = '{"msg":"The user already exists"}';
+            returnJson($user_exists_msg);
+            die(1); // the objectively cooler version of exit
+        }
+    }
+
+    function returnJson($obj) {
         header('Content-type: application/json');
-        $success_message = sprintf('{"username":"%s","password":"%s"}', $username, $password);
-        echo $success_message;
+        echo $obj;
     }
     /*
     Sign-up Flow
+        1. check if the user is in the database
+            1.1 yes -> send a message saying it already exists
+            1.2 no -> make a new row with the passed username and password
+                send a message informing the user has been created successfully
     */
 ?>
