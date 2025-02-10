008db2a43c6549f7ee319c690e1cca53206f4b52
nelson-ricardo <54825870+nelson-ricardo@users.noreply.github.com>
2025-02-10 22:20:24 +0000

    pls work

--- LAMPAPI/signup.php
+++ LAMPAPI/signup.php
@@ -7,26 +7,9 @@
     // make a connection to the database
     $db_connection = new mysqli("localhost", "Admin", "administratorpriveleges", "Contact");
 
-    if( $db_connection->connect_error ) {
-        $error_json_msg = '{"msg":"WOMP WOMP"}';
-        returnJson($error_json_msg);
-    } else {
-        // TODO: Figure out why there is 500 Internal Server Error
-        /*
-        Possible solution, maybe use bind_params() method instead of sprintf to bind the parameters to the statement
-        */
-        // This follows the prepare - bind - execute model for SQL queries in PHP please work I am going to crash out
-        $existence_stmt = db_connection->prepare("SELECT * FROM Users WHERE username = ?");
-        returnMsg("preparing successful! ^-^");
-        $existence_stmt->bind_param("s", $username);
-        returnMsg("binding successful! ^-^");
-        $existence_stmt->execute();
-        returnMsg("executing successful! ^-^");
-        $existence_result = $existence_stmt->get_result();
-        returnMsg("fetching successful! ^-^");
-        $query_result = $existence_result->fetch_assoc();
-        returnMsg("setting to associative array successful! ^-^");
-    }
+    $exist_stmt = db_connection->prepare("SELECT COUNT(*) as num_users FROM Users WHERE username = ?");
+    $exist_stmt->bind_param("s", $username);
+    $exist_stmt->execute();
 
     function returnJson($obj) {
         header('Content-type: application/json');
