ed1af1eef2457ee35de21d76ef12a31673af04a9
Nelson Oliva <nelson.ricardo02@gmail.com>
2025-02-14 16:42:21 -0500

    update username and endpoint name

--- js/code.js
+++ js/code.js
@@ -31,7 +31,7 @@ function doLogin()
 //	var tmp = {login:login,password:hash};
   let jsonPayload = JSON.stringify(tmp);
 
-  let url = urlBase + '/Login.' + extension;
+  let url = urlBase + '/signin.' + extension;
 
   let xhr = new XMLHttpRequest();
   xhr.open("POST", url, true);
