f13c2580ab486cea80c555734e239195a394f517
Nelson Oliva <nelson.ricardo02@gmail.com>
2025-02-26 12:41:19 -0500

    add split limit of 1

--- LAMPAPI/searchcontact.php
+++ LAMPAPI/searchcontact.php
@@ -87,10 +87,10 @@
 
         while($row = $sql_res->fetch_assoc()) {
             if(str_contains($row["firstName"], $string)) {
-                $old_string = explode(strtoupper($string), strtoupper($row["firstName"]));
+                $old_string = explode(strtoupper($string), strtoupper($row["firstName"]), 1);
                 $row["firstName"] = $old_string;
             } else {
-                $old_string = explode(strtoupper($string), strtoupper($row["lastName"]));
+                $old_string = explode(strtoupper($string), strtoupper($row["lastName"]), 1);
                 $row["lastName"] = $old_string;
             }
             array_push($retArray, $row);
