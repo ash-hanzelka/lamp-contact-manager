5ed9a57457c1c376fec380e56801aa9e1f2090b0
Marco Osorio <marcoosorio109@gmail.com>
2025-02-26 21:35:50 -0500

    delete by contactId

--- LAMPAPI/deletecontact.php
+++ LAMPAPI/deletecontact.php
@@ -1,39 +1,22 @@
 <?php
     $inData = json_decode(file_get_contents("php://input"), true);
 
-    $userId = (int)$inData["userId"];
-    if (!$userId) {
-        returnError("No userId");
-    }
-    $firstName = $inData["firstName"];
-    if (!$firstName) {
-        returnError("No first name");
-    }
-    $lastName = $inData["lastName"];
-    if (!$lastName) {
-        returnError("No last name");
-    }
-    $email = $inData["email"];
-    if (!$email) {
-        returnError("No email address");
-    }
-    $phone = $inData["phone"];
-    if (!$phone) {
-        returnError("No phone number");
+    $contactId = (int)$inData["contactId"];
+    if (!$contactId) {
+        returnError("No contactId");
     }
 
     $conn = new mysqli("localhost", "theManager", "ContactManager", "Contact");
-
     
     if ($conn->connect_error) {
         returnError($conn->connect_error);
     } else {
-        $stmt = $conn->prepare("DELETE FROM Contacts WHERE userid = ? AND firstName = ? AND lastName = ? AND email = ? AND phone = ?");
+        $stmt = $conn->prepare("DELETE FROM Contacts WHERE contactId = ?");
         if (!$stmt) {
             returnError("Prepare failed: " . $conn->error);
             exit;
         }
-        $stmt->bind_param("issss", $userId, $firstName, $lastName, $email, $phone);
+        $stmt->bind_param("i", $contactId);
         $stmt->execute();
 
         if ($conn->affected_rows > 0) {
